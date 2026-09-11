2694e5a3c3cdf0a155bfddde8e2c386bc20a9da8
7.studio <mail@julianejeske.com>
2026-09-11 17:53:43 +0200

    refactored wrong GlobalStyle import

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,5 @@
-import { createGlobalStyle } from "styled-components";
+import { GlobalStyle } from "../styles";
 
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    background-color: #f2f2f2;
-    font-family: Arial, sans-serif;
-    background-repeat: repeat;
-    background-size: 600px 300px;
-  }
-`;
 export default function App({ Component, pageProps }) {
   return (
     <>
