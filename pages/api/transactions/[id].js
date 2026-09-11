aaf2b470950c7833cdc28558daac2402b8857bc1
7.studio <mail@julianejeske.com>
2026-09-11 18:42:14 +0200

    refactored api transactionData request

--- pages/api/transactions/[id].js
+++ pages/api/transactions/[id].js
@@ -10,7 +10,7 @@ export default async function handler(request, response) {
       // Data sent by the frontend
       const transactionData = request.body;
 
-      await Project.findByIdAndUpdate(
+      const transaction = await Project.findByIdAndUpdate(
         id,
         transactionData
       );
